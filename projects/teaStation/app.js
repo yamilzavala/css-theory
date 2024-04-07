dd1494155fda6309ed744f7c6e83767e93a36139
yamilzavala <zavalayamil9@gmail.com>
2024-04-07 11:22:37 -0300

    service - contact - footer - sections done

--- projects/teaStation/app.js
+++ projects/teaStation/app.js
@@ -3,14 +3,15 @@ const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
 // show nav
-navBtn.addEventListener("click", () => {
+navBtn?.addEventListener("click", () => {
   navbar.classList.add("showNav");
 });
 // close nav
-navClose.addEventListener("click", () => {
+navClose?.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
 //setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+const dateEl = document.querySelector('#date');
+dateEl.innerHTML = new Date().getFullYear();
+
+
