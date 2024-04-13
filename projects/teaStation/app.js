2a792429c143cd22d4fb3565415b3a1e2c806076
yamilzavala <zavalayamil9@gmail.com>
2024-04-13 08:45:27 -0300

    add animations and navbar panel

--- projects/teaStation/app.js
+++ projects/teaStation/app.js
@@ -2,16 +2,19 @@
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
+
 // show nav
-navBtn?.addEventListener("click", () => {
-  navbar.classList.add("showNav");
-});
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('showNav')
+})
 // close nav
-navClose?.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('showNav')
+})
+
 //setup date
 const dateEl = document.querySelector('#date');
 dateEl.innerHTML = new Date().getFullYear();
+
 
 
